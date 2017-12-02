aea4741976a6bc24e93c8dcd23a379446e54e498
Yuan <yuanji.yj@gmail.com>
2017-12-02 11:20:07 -0500

    added some redux form fields

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Field, reduxForm } from 'redux-form';
+
+const onSubmit = (data) => {
+  console.log(data)
+}
+
+const customField = (field) => {
+  return (
+    <div>
+      <input {...field.input} type="text"/>
+      {field.meta.touched && field.meta.error && 
+      <span>{field.meta.error}</span>}
+    </div>
+  )
+}
+
+const ContactForm = props => {
+  const { handleSubmit } = props;
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="nativeField">Native Field</label>
+        <Field name="nativeField" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="customField">Custom Field</label>
+        <Field name="customField" component={customField} />
+      </div>
+      <input type="submit" />
+    </form>
+  )
+}
+
+const ContactFormReduxForm = reduxForm({
+  form: 'contact'
+})(ContactForm);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Redux Form Experiment</h1>
+        <ContactFormReduxForm />
+      </div>
+    );
+  }
+}
+
+export default App;
